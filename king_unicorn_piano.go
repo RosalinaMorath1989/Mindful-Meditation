c2826f3ce3fb933b0a33ffb4fdae5e558f1a63e7
RosalinaMorath1989 <137514068+RosalinaMorath1989@users.noreply.github.com>
2023-06-29 05:20:30 +0700

    Refactor code

---
+++ king_unicorn_piano.go
@@ -0,0 +1,74 @@
+package mindful_meditation
+
+import (
+    "time"
+    "fmt"
+)
+
+func main() {
+    currentTime := time.Now()
+
+    // Print out the current time
+    fmt.Println("It is currently", currentTime.Format("3:04PM"))
+
+    // Begin the meditation session
+    fmt.Println("Let us begin our meditation session.")
+
+    // Close your eyes and focus on your breathing
+    fmt.Println("Close your eyes and focus on your breathing.")
+
+    // Take a deep breath and exhale slowly
+    fmt.Println("Take a deep breath and exhale slowly.")
+
+    //Bring your attention to your lower abdomen
+    fmt.Println("Bring your attention to your lower abdomen.")
+
+    // Feel every inhalation and exhalation
+    fmt.Println("Feel every inhalation and exhalation.")
+
+    // Notice any sensations and thoughts
+    fmt.Println("Notice any sensations and thoughts.")
+
+    // Be aware of the passing moment
+    fmt.Println("Be aware of the passing moment.")
+
+    // Count your breaths from one to five
+    fmt.Println("Count your breaths from one to five.")
+
+    // Acknowledge the distractions
+    fmt.Println("Acknowledge the distractions.")
+
+    // Refocus your attention on your breath
+    fmt.Println("Refocus your attention on your breath.")
+
+    //Relax your body and let go of any tensions
+    fmt.Println("Relax your body and let go of any tensions.")
+
+    //Release any thoughts or feelings
+    fmt.Println("Release any thoughts or feelings.")
+
+    //Notice any emotions and be mindful of them
+    fmt.Println("Notice any emotions and be mindful of them.")
+
+    //Focus on the present and the here and now
+    fmt.Println("Focus on the present and the here and now.")
+
+    //Take a few more breaths and bring awareness to your body
+    fmt.Println("Take a few more breaths and bring awareness to your body.")
+
+    //Slowly open your eyes and take a moment to observe
+    fmt.Println("Slowly open your eyes and take a moment to observe.")
+
+    //Take some time to reflect on how you feel
+    fmt.Println("Take some time to reflect on how you feel.")
+
+    //Conclude the meditation session
+    fmt.Println("Let us conclude our meditation session.")
+
+    //Thank yourself for taking the time
+    fmt.Println("Thank yourself for taking the time to be mindful.")
+
+    //Allow yourself to feel refreshed and energized
+    fmt.Print("Allow yourself to feel refreshed and energized.")
+
+}
